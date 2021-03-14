b9d83404e31ea790b46ce5fb6af6cf174f5e043d
VHenriksson <v-henriksson@hotmail.com>
2021-03-14 09:25:46 +0000

    Changed Cmake Version

---
+++ CMakeFiles/GameEngine.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viktor/CLionProjects/GameEngine/GameEngine.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/GameEngine.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/GamePartBase.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/GamePartBase.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/InputManager/InputManagerBase.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/InputManager/InputManagerBase.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/InputManager/InputManagerCaterpillar.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/InputManager/InputManagerCaterpillar.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/GLMesh/GLMeshBase.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/GLMesh/GLMeshBase.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/GLMesh/GLMeshColour.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/GLMesh/GLMeshColour.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/GLMesh/GLMeshTexture.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/GLMesh/GLMeshTexture.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/GLMesh/ImportedMesh.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/GLMesh/ImportedMesh.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/Geometry.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/Geometry.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/Importer.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/Importer.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/Materials/Material.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/Materials/Material.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/Object.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/Object.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/PhongShadedGeometry.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/PhongShadedGeometry.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Objects/TexturedGeometry.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Objects/TexturedGeometry.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GameScenes/CaterpillarScene.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GameScenes/CaterpillarScene.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GameScenes/GameObjects/Caterpillar.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GameScenes/GameObjects/Caterpillar.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GameScenes/GameObjects/Food.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GameScenes/GameObjects/Food.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GameScenes/GameObjects/PhongShadedObject.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GameScenes/GameObjects/PhongShadedObject.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GameScenes/GameObjects/Planet.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GameScenes/GameObjects/Planet.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GameScenes/GameObjects/Plupp.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GameScenes/GameObjects/Plupp.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/GeometryList.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/GeometryList.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/RenderingList.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/RenderingList.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Scenes/Scene.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Scenes/Scene.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Shading/PhongShader.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Shading/PhongShader.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Shading/ShaderLoader.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Shading/ShaderLoader.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Shading/ShaderProgramBase.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Shading/ShaderProgramBase.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/Shading/TexturePhongShader.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/Shading/TexturePhongShader.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/WindowHandling/GLContext.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/WindowHandling/GLContext.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/WindowHandling/GLwindow.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/WindowHandling/GLwindow.cpp.o"
+  "/home/viktor/CLionProjects/GameEngine/main.cpp" "/home/viktor/CLionProjects/GameEngine/CMakeFiles/GameEngine.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,3 @@
+# GameEngine
+
+A simple game engine used to create a game inspired by Snake, but on a 3D-sphere. The engine uses OpenGL and SDL and relies on Assimp and stb for model loading.
